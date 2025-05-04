ac6b3face3ced94fb0ef3a122d6cc5c40b4210d6
EduardoStarZ <pathver@gmail.com>
2025-05-03 21:44:29 -0300

    yazi: changed image viewer to mirage; scripts: some scripts for dependencies

---
+++ deps.sh
@@ -0,0 +1,3 @@
+sudo pacman -Sy neovim ripgrep ffmpeg i3gaps ly yazi thunar gvfs dunst polybar rofi git github-cli 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
